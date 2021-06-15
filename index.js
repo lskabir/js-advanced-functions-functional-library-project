924aac64cb5a04d84263dd79c0807cc3dad1313f
github username <github email address>
2021-06-14 20:40:35 -0400

    commit tree 1e7ca742b18443a8bade468f25fe9efab347f87c

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
+      for (let i = 0; i < newCollection.length; i++) {
+        callback(newCollection[i])
+      }
+      return collection
     },
 
-    map: function() {
+    map: function(collection, callback) {
+      let newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+      const newArr = []
+
+      for (let i = 0; i < newCollection.length; i++) {
+        newArr.push(callback(newCollection[i]))
+      }
+      return newArr
     },
 
     reduce: function() {
